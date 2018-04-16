aa795ad8df7ea9bc00662e81a0a8b29c16276349
restuu <restu_u@outlook.com>
2018-04-16 17:33:11 +0700

    release 3 done

--- query.js
+++ query.js
@@ -14,4 +14,66 @@ function delete_politicians(id_value) {
   db.run(`DELETE FROM politicians WHERE id = $id`, {$id: id_value})
 }
 
+// insert_politicians('Mamat', 'Terong', 'Ds Il', 99.999);
+// update_politicians('name', 'John', 21)
+// delete_politicians(21)
+
+
+class Query {
+  constructor() {
+
+  }
+
+  static select(col_name, table_name, condition) {
+    db.all(`SELECT ${col_name} FROM ${table_name} WHERE ${condition}`, (err, output_by_party) => {
+      console.log(output_by_party);
+    })
+  }
+
+  static showOlympiaSnowe(){
+    db.get(`SELECT COUNT(*) AS 'totalVote', politicians.name
+            FROM votes
+            LEFT JOIN politicians
+            ON votes.politicianId = politicians.id
+            WHERE politicians.name = 'Olympia Snowe'`, (err, olympia_snowe) => {
+      console.log(olympia_snowe);
+    })
+  }
+
+  static show3Politicians(){
+    db.all(`SELECT COUNT(*) AS 'totalVote', politicians.name, politicians.party, politicians.location
+            FROM votes
+            LEFT JOIN politicians
+              ON votes.politicianId = politicians.id
+            GROUP BY politicians.name
+            ORDER BY totalVote DESC
+            LIMIT 3`, (err, politicians) => {
+      console.log(politicians);
+    })
+  }
+
+  static whoVoteOlympiaSnowe() {
+    let innerQuery = `SELECT *
+                      FROM votes
+                      LEFT JOIN politicians
+                        ON votes.politicianId = politicians.id
+                      WHERE politicians.name = 'Olympia Snowe'`
+    db.all(`WITH inner AS (${innerQuery})
+            SELECT first_name, last_name, gender, age
+            FROM inner
+            LEFT JOIN voters
+              ON inner.voterId = voters.id`, (err, voters) => {
+      console.log(voters);
+      console.log(err);;
+    })
+  }
+}
+
+let condition = "grade_current BETWEEN 9 AND 11 AND party = 'R'"
+// Query.select('name, party, grade_current', 'politicians', condition);
+// Query.showOlympiaSnowe()
+// Query.show3Politicians()
+Query.whoVoteOlympiaSnowe()
+
+
 module.exports = {insert_politicians, update_politicians, delete_politicians}
