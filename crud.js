9f2340c09f0dc3eb9e6025ca41ccca01089a9466
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2018-04-16 15:40:43 +0700

    release-2

---
+++ crud.js
@@ -0,0 +1,80 @@
+"use strict"
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('database.db');
+
+class Crud {
+  static insertData(tableName, objData) {
+    let query;
+
+    if (tableName === 'Politicians') {
+      query = `INSERT INTO Politicians
+               VALUES (NULL, "${objData.name}", "${objData.party}", "${objData.location}", "${objData.grade_current}");`;
+    } else if (tableName === 'Voters') {
+      query = `INSERT INTO Voters
+               VALUES (NULL, "${objData.first_name}", "${objData.last_name}", "${objData.gender}", "${objData.age}");`;
+    } else if (tableName === 'Votes') {
+      query = `INSERT INTO Votes
+               VALUES (NULL, "${objData.voterId}", "${objData.politicianId}");`;
+    }
+
+    db.run(query, data => {
+      console.log('Data berhasil dimasukkan');
+    });
+  }
+
+  static updateData(tableName, column, value, id) {
+    let query;
+
+    if (tableName === 'Politicians') {
+      query = `UPDATE Politicians
+               SET "${column}" = "${value}"
+               WHERE id = ${id};`;
+    } else if (tableName === 'Voters') {
+      query = `UPDATE Voters
+               SET "${column}" = "${value}"
+               WHERE id = ${id};`;
+    } else if (tableName === 'Votes') {
+      query = `UPDATE Votes
+               SET "${column}" = "${value}"
+               WHERE id = ${id};`;
+    }
+
+    db.run(query, data => {
+      console.log('Data berhasil diperbaharui');
+    });
+  }
+
+  static deleteData(tableName, id) {
+    let query;
+
+    if (tableName === 'Politicians') {
+      query = `DELETE FROM Politicians
+               WHERE id = ${id};`;
+    } else if (tableName === 'Voters') {
+      query = `DELETE FROM Voters
+               WHERE id = ${id};`;
+    } else if (tableName === 'Votes') {
+      query = `DELETE FROM Votes
+               WHERE id = ${id};`;
+    }
+
+    db.run(query, data => {
+      console.log('Data berhasil dihapus');
+    });
+  }
+}
+
+// INSERT Data to database.db
+// Crud.insertData('Politicians', {name: 'Ihsan', party: 'M', location: 'RM', grade_current: '13.73585922'});
+// Crud.insertData('Voters', {first_name: 'Ihsan', last_name: 'Maulana', gender: 'male', age: 21});
+// Crud.insertData('Votes', {voterId: '152', politicianId: '7'});
+
+// UPDATE Data to database.db by id
+// Crud.updateData('Politicians', 'name', 'Ihsan Maulana', 21);
+// Crud.updateData('Voters', 'age', 22, 151);
+// Crud.updateData('Votes', 'voterId', '151', 164);
+
+// DELETE Data to database.db by id
+// Crud.deleteData('Politicians', 21);
+// Crud.deleteData('Voters', 151);
+// Crud.deleteData('Votes', 164);
